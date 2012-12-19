919ae187df0f816099acc32d928a3e9b6e394e82
vub <vub@gmail.com>
2012-12-19 10:31:38 -0500

    Debugged webserver; adding and getting messages works in the abstract, integration with proposal and offer objects still remains

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-import web, json
 
-urls = ('/messages', 'json')
+import web, json
+import time
+import random
+
+
+urls = ('/messages', 'jsonapi')
 
 app = web.application(urls, globals())
 
 class TextfileDatabaseHandler():
     def __init__(self,filename):
-        self.data = json.loads(open(filename,'r').read())
+        try:
+            self.data = json.loads(open(filename,'r').read())
+        except:
+            open(filename,'w').write('')
+            fileobj = open(filename,'r')        
+            self.data = {}
         self.filename = filename
     def save(self):
         open(self.filename,'w').write(json.dumps(self.data))
-    def get(key):
-        return self.data.get(key,None)
+    def get(self,key):
+        return self.data.get(str(key),None)
     def listkeys(self):
         return self.data.keys()
-    def put(key,val,save=True):
+    def put(self,key,val,save=True):
         self.data[key]=val
         if save: self.save()
+
+db = TextfileDatabaseHandler('db.txt')
 
 def serialize(obj):
     # Should convert object to string representation
@@ -26,22 +37,26 @@ def deserialize(data):
     # Should convert string representation to object
     return data
 
-class json:
-    db = TextfileDatabaseHandler('db.txt')
-    def GET(self,id=None):
-        if id: 
-            data = db.get(id)
+class jsonapi:
+    def GET(self):
+        id = web.input(id=None).id
+        print id
+        if id is not None: 
+            data = db.get(id) or []
         else:
-            data = reduce(lambda x,y:x+y, [db.get(k) for k in db.listkeys()])
-        print json.dumps(sorted(data,key=lambda x:-x['timestamp'])[:25])
+            data = reduce(lambda x,y:x+y, [[]]+[db.get(k) for k in db.listkeys()])
+        return json.dumps(sorted(data,key=lambda x:-x['timestamp'])[:25])
         
     def POST(self):
         data = web.data()
         try:
             obj = deserialize(data)
-            try: id = obj.oid
-            except: id = obj.offer.oid
-            db.put(id,data)
+            try: id = str(obj.oid)
+            except:
+              try: id = str(obj.offer.oid)
+              except: id = "0"
+            db.put(id,(db.get(id) or '[]') + \
+                [{'id':id,'timestamp':int(time.time()),'content':data}])
             return 'Success'
         except:
             return 'Failure'
