7537fac74c67b74010d2f66d39cb6aac85b58ff9
vub <vub@gmail.com>
2012-12-25 21:38:16 -0500

    Replaced text file database with sqlite3

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 import web, json
 import time
+import os
+import sqlite3
 
 from p2ptrade import *
 
@@ -8,59 +10,55 @@ urls = ('/messages', 'jsonapi')
 
 app = web.application(urls, globals())
 
-class TextfileDatabaseHandler():
-    def __init__(self,filename):
-        try:
-            self.data = json.loads(open(filename,'r').read())
-        except:
-            open(filename,'w').write('')
-            fileobj = open(filename,'r')        
-            self.data = {}
-        self.filename = filename
-    def save(self):
-        open(self.filename,'w').write(json.dumps(self.data))
-    def get(self,key):
-        return self.data.get(str(key),None)
-    def listkeys(self):
-        return self.data.keys()
-    def put(self,key,val,save=True):
-        self.data[str(key)]=val
-        if save: self.save()
+def initdb(filename):
+    try:
+       d,f = os.path.split(filename)
+       assert f in os.listdir(os.path.join(os.getcwd(),d))
+       exists = True
+    except:
+       exists = False
+    c = sqlite3.connect(filename)
+    if not exists:
+       print 'Creating database tables'
+       c.execute('''CREATE TABLE messages
+           (id bigint, timestamp bigint, serial bigint, content text)''')
+       c.execute('''CREATE TABLE metadata
+           (key text, value text)''')
+       c.commit()
 
-db = TextfileDatabaseHandler('db.txt')
+initdb('main.db')
 
 class jsonapi:
     def GET(self):
+        conn = sqlite3.connect('main.db')
         id = web.input(id=None).id
         from_timestamp = int(web.input(from_timestamp=-1).from_timestamp)
         from_serial = int(web.input(from_serial=-1).from_serial)
-        data = db.get('messages') or []
-        maxnum = 25 if from_timestamp == -1 and from_serial == -1 else 9999
-        def filterf(x):
-          if id is not None and x.get('id') != id: return False
-          if x.get('timestamp',0) < from_timestamp: return False
-          if x.get('serial',0) < from_serial: return False
-          return True
-        data = filter(filterf,data)
+        maxnum = 25 if from_timestamp == -1 and from_serial == -1 else 99999
+        if id is not None:
+          rawdata = conn.execute('SELECT * From messages WHERE id = ? AND serial >= ? AND timestamp >= ?',
+                     (id, from_serial, from_timestamp)).fetchall()
+        else:
+          rawdata = conn.execute('SELECT * From messages WHERE serial >= ? AND timestamp >= ?',
+                     (from_serial, from_timestamp)).fetchall()
+        data = [{'id':x[0],'timestamp':x[1],'serial':x[2],'content':x[3]} for x in rawdata]
         return json.dumps(sorted(data,key=lambda x:-x['timestamp'])[:maxnum])
         
     def POST(self):
+        conn = sqlite3.connect('main.db')
         data = web.data()
         try:
-            print data
             obj = json.loads(data)
-            try: id = str(obj["oid"])
+            try: id = obj["oid"]
             except:
-              try: id = str(obj["offer"]["oid"])
-              except: id = "0"
-            print 0
-            serial = db.get('last_serial') or 0
-            print 1
-            existing = db.get('messages') or []
-            print 2
-            new = [{'id':id,'timestamp':int(time.time()),'serial':serial+1,'content':obj}]
-            db.put('last_serial',serial+1,False)
-            db.put('messages',existing + new)
+              try: id = obj["offer"]["oid"]
+              except: id = 0
+            serial = int((conn.execute('SELECT * From metadata WHERE key = "last_serial"').fetchone() or [0,0])[1])
+            new = (id, int(time.time()),serial+1,data)
+            conn.execute('DELETE From metadata WHERE key = "last_serial"')
+            conn.execute('INSERT INTO metadata VALUES (?,?)',("last_serial",serial+1))
+            conn.execute('INSERT INTO messages VALUES (?,?,?,?)',new)
+            conn.commit()
             return 'Success'
         except:
             return 'Failure'
