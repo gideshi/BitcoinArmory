9076996d63433d79ca101f9b1e1974a25850688f
Alex Mizrahi <alex.mizrahi@gmail.com>
2012-12-27 14:12:16 +0000

    p2ptrade-test

---
+++ extras/p2ptrade-test.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append('..')
+sys.path.append('.')
+
+from armoryengine import *
+import connector
+
+import p2ptrade
+import colortools
+
+connector.init()
+
+class EchoExchangeComm:
+    def __init__(self):
+        self.agents = []
+    def addAgent(self, agent):
+        self.agents.append(agent)
+    def postMessage(self, content):
+        print content
+        for a in self.agents:
+            a.dispatchMessage(content)
+
+comm = EchoExchangeComm()
+
+def mkagent(wfile):
+    wlt = PyBtcWallet().readWalletFile(wfile)
+    connector.register_wallet(wlt)
+    a = p2ptrade.ExchangePeerAgent(wlt, comm)
+    comm.addAgent(a)
+    return a
+
+ag1 = mkagent(CLI_ARGS[0])
+ag2 = mkagent(CLI_ARGS[1])
+
+testcolor = '26546d600e1a6a278eba2170559afe415ddcdd88'
+uncolored = ''
+
+o1 = p2ptrade.ExchangeOffer(None, {"value": 100, "colorid": testcolor}, {'value': 100, 'colorid': uncolored})
+o2 = p2ptrade.ExchangeOffer(None, {"value": 100, "colorid": uncolored}, {"value": 100, 'colorid': testcolor})
+
+ag1.registerMyOffer(o1)
+ag2.registerMyOffer(o2)
+
+ag1.postMessage(o1)
+
