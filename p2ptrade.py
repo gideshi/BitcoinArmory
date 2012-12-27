b92fa1598691c61bcf88d650c671362038eac7c7
Alex Mizrahi <alex.mizrahi@gmail.com>
2012-12-27 14:11:57 +0000

    p2ptrade fixes

--- p2ptrade.py
+++ p2ptrade.py
@@ -7,7 +7,7 @@ import threading, time
 
 def make_random_id():
     bits = os.urandom(8)
-    return bin_to_hex(bits)
+    return binary_to_hex(bits)
 
 def colorInd(o): return colortools.find_color_index(o['colorid'])
 
@@ -43,10 +43,10 @@ class ExchangeOffer:
         def checkprop(name):
             if self.A[name] != my_offer.A[name]: return False
             if self.B[name] != my_offer.B[name]: return False
+            return True
 
-        if not checkprop('colorid'): return False
+        if not checkprop('colorid'):  return False
         if not checkprop('value'): return False
-
         return True
 
     @classmethod
@@ -57,24 +57,25 @@ class ExchangeOffer:
 class MyTranche(object):
     @classmethod
     def createPayment(cls, wallet, color, amount, to_address160):
-        p = MyETransactionTranche()
+        p = MyTranche()
         p.txdp = None
         p.utxoList = wallet.getTxOutListX(color, 'Spendable')
         p.utxoSelect = PySelectCoins(p.utxoList, amount, 0)
         if p.utxoSelect:
             totalSelectCoins = sum([u.getValue() for u in p.utxoSelect])
             change = totalSelectCoins - amount
-            self.recipientPairs   = [[to_address, amount]]
+            p.recipientPairs   = [[to_address160, amount]]
             if change > 0:
-                addr = self.utxoSelect[0].getRecipientAddr()
+                addr = p.utxoSelect[0].getRecipientAddr()
                 # TODO: check invalid addr?
                 p.recipientPairs.append([addr, change])
+            return p
         else:
             raise Exception("not enough money(?)")
 
     def makeTxDistProposal(self):
         txdp = PyTxDistProposal()
-        txdp.createFromTxOutSelection(p.utxoSelect, p.recipientPairs)
+        txdp.createFromTxOutSelection(self.utxoSelect, self.recipientPairs)
         return txdp
 
 
@@ -86,7 +87,9 @@ def merge_txdps(l, r):
     res.isSigned = False
     res.lockTime = 0
     res.thisHash = res.getHash()
-    return res
+    p = PyTxDistProposal()
+    p.createFromPyTx(res)
+    return p
     
 
 class ExchangeTransaction:
@@ -109,6 +112,7 @@ class ExchangeTransaction:
 
     def broadcast(self):
         finalTx = self.getTxDP().prepareFinalTx()
+        finalTx.pprint()
         engine_broadcast_transaction(finalTx)
 
     def getASCIITxDP(self):
@@ -132,7 +136,7 @@ class ExchangeProposal:
         self.pid = data["pid"]
         self.offer = ExchangeOffer.importTheirs(data["offer"])
         self.etransaction = ExchangeTransaction()
-        txdp = PyTransactionDistProposal()
+        txdp = PyTxDistProposal()
         txdp.unserializeAscii(data['txdp'])
         self.etransaction.addTxDP(txdp)
         self.my_tranche = None
@@ -145,11 +149,12 @@ class ExchangeProposal:
     # that I want going to my address?
     def checkOutputsToMe(self,myaddress,color,value):
         txdp = self.etransaction.getTxDP()
-        coloredOutputs = colortools.compute_pytx_colors(txdp)
+        coloredOutputs = colortools.compute_pytx_colors(txdp.pytxObj)
+        print coloredOutputs
         sumv = 0
         for out,col in zip(txdp.pytxObj.outputs,coloredOutputs):
           if TxOutScriptExtractAddr160(out.binScript) == myaddress:
-            if col[1] == color:
+            if col == color:
               sumv += out.value
         offer = self.offer
         return sumv >= value
@@ -172,18 +177,20 @@ class ExchangeProposal:
         
     def signMyTranche(self,wallet):
         if self.checkInputsFromMe(wallet):
-           wallet.signTxDistProposal(self.extransaction.txdp)
+           wallet.signTxDistProposal(self.etransaction.txdp)
 
 class ExchangePeerAgent:
-    def __init__(self, wallet):
+    def __init__(self, wallet, comm):
         self.my_offers = dict()
         self.their_offers = dict()
         self.wallet = wallet
         self.eproposals = dict()
         self.lastpoll = 0
-        self.pollingloop()
+        self.comm = comm
 
     def registerMyOffer(self, offer):
+        if not 'address' in offer.A:
+            offer.A['address'] = self.wallet.getNextUnusedAddress().getAddr160()
         self.my_offers[offer.oid] = offer
         self.matchOffers()
 
@@ -192,8 +199,8 @@ class ExchangePeerAgent:
         self.matchOffers()
 
     def matchOffers(self):
-        for my_offer in self.my_offers:
-            for their_offer in self.their_offers:
+        for my_offer in self.my_offers.itervalues():
+            for their_offer in self.their_offers.itervalues():
                 if my_offer.matches(their_offer):
                     self.makeExchangeProposal(their_offer, my_offer.A['address'], my_offer.A['value'])
 
@@ -205,7 +212,8 @@ class ExchangePeerAgent:
         if not acolor or not bcolor:
             raise Exception("My colorid is not recognized")
         my_tranche = MyTranche.createPayment(self.wallet, bcolor, my_value, offer.A['address'])
-        ep = ExchangeProposal(offer, my_tranche)
+        ep = ExchangeProposal()
+        ep.createNew(offer, my_tranche)
         self.eproposals[ep.pid] = ep
         self.postMessage(ep)
 
@@ -214,7 +222,7 @@ class ExchangePeerAgent:
         ep.importTheirs(ep_data)
         if ep.offer.oid in self.my_offers:
           return self.acceptExchangeProposal(ep)
-        elif ep.offer.pid in self.eproposals:
+        elif ep.pid in self.eproposals:
           return self.updateExchangeProposal(ep)
         else: 
           # We have neither an offer nor a proposal matching this ExchangeProposal
@@ -228,21 +236,27 @@ class ExchangePeerAgent:
         my_offer = self.my_offers[offer.oid]
         if not offer.isSameAsMine(my_offer):
             raise Exception("Is invalid or incongruent with my offer")
-        if not ep.checkOutputsToMe(offer.A['address'],offer.B['color'],offer.B['value']):
+        acolor, bcolor = colorInd(offer.A), colorInd(offer.B)
+        if not acolor or not bcolor:
+            raise Exception("My colorid is not recognized")
+        if not ep.checkOutputsToMe(offer.A['address'], bcolor, offer.B['value']):
             raise Exception("Offer does not contain enough coins of the color that I want for me")
-        ep.addMyTranche(MyTranche.createPayment(self.wallet, offer.A['color'], offer.A['value'], offer.B['address']))
+        ep.addMyTranche(MyTranche.createPayment(self.wallet, acolor, offer.A['value'], offer.B['address']))
         ep.signMyTranche(self.wallet)
         self.eproposals[ep.pid] = ep
         self.postMessage(ep)
 
     def updateExchangeProposal(self, ep):
-        my_ep = self.eproposals.get([ep.pid], None)
+        my_ep = self.eproposals.get(ep.pid, None)
         if my_ep.sealed:
             # cannot update sealed EP
             return
         offer = my_ep.offer
         ep.my_tranche = my_ep.my_tranche
-        if not ep.checkOutputsToMe(offer.B['address'],offer.A['color'],offer.A['value']): 
+        acolor, bcolor = colorInd(offer.A), colorInd(offer.B)
+        if not acolor or not bcolor:
+            raise Exception("My colorid is not recognized")
+        if not ep.checkOutputsToMe(offer.B['address'], acolor, offer.A['value']): 
             raise Exception("Offer does not contain enough coins of the color that I want for me")
         ep.signMyTranche(self.wallet)
         if not (ep.etransaction.getTxDP().checkTxHasEnoughSignatures()):
@@ -251,9 +265,23 @@ class ExchangePeerAgent:
         ep.etransaction.broadcast()
 
 
-    def postMessage(self,obj):
+    def postMessage(self, obj):
+        self.comm.postMessage(obj.export())
+
+    def dispatchMessage(self, content):
+        if 'oid' in content:
+            o = ExchangeOffer(content['oid'],content['A'],content['B'])
+            self.registerTheirOffer(o)
+        elif 'pid' in content:
+            self.dispatchExchangeProposal(content)
+
+class HTTPExchangeComm:
+    def __init__(self, agent):
+        self.agent = agent
+
+    def postMessage(self, content):
         h = httplib.HTTPConnection('localhost:8080')
-        data = json.dumps(obj.export())
+        data = json.dumps(content)
         h.request('POST', '/messages', data, {})
         return k.getresponse().read() == 'Success'
 
@@ -264,19 +292,12 @@ class ExchangePeerAgent:
             resp = json.loads(h.getresponse().read())
             for x in resp:
                 if x.get('serial') > lastpoll: lastpoll = x.get('serial')
-                content = x.get('content')
-                if 'oid' in content:
-                    o = ExchangeOffer(content['oid'],content['A'],content['B'])
-                    self.registerTheirOffer(o)
-                elif 'pid' in content:
-                    self.dispatchExchangeProposal(content)
-                else:
-                    return False
+                agent.dispatchMessage(x.get('content'))
             return True      
         except:
             return False
 
-    def pollingloop(self):
+    def pollingLoop(self):
         def infipoll():
             while 1:
                 time.sleep(15)
